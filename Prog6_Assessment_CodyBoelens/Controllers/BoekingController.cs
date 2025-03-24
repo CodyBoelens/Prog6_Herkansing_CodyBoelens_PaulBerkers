d50b244d14ab55edb4b9c091b1b6467a2fc5e402
PaulBerkersAvans <130966922+PaulBerkersAvans@users.noreply.github.com>
2025-03-24 14:40:56 +0100

    added saving in session between steps

--- Prog6_Assessment_CodyBoelens/Controllers/BoekingController.cs
+++ Prog6_Assessment_CodyBoelens/Controllers/BoekingController.cs
@@ -8,6 +8,7 @@ using Prog6_Assessment_CodyBoelens.Views.ViewModels.KlantViewModel;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
+using Prog6_Assessment_CodyBoelens.Views.ViewModels.BeestjeViewModel;
 
 namespace Prog6_Assessment_CodyBoelens.Controllers
 {
@@ -26,7 +27,7 @@ namespace Prog6_Assessment_CodyBoelens.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return RedirectToAction("Index", "Home");
         }
 
         [HttpPost]
@@ -48,12 +49,7 @@ namespace Prog6_Assessment_CodyBoelens.Controllers
         public async Task<IActionResult> Step02()
         {
             DateTime eventDate = getEventDateFromSession();
-
-            if (eventDate == default)
-            {
-                // Redirect if no event date is found
-                return RedirectToAction("Index", "Home");
-            }
+            if (eventDate == default) return RedirectToAction("Index", "Home");
 
             ViewBag.EventDate = eventDate;
 
@@ -67,28 +63,26 @@ namespace Prog6_Assessment_CodyBoelens.Controllers
                     // Fetch the klant using the ApplicationUserId
                     var klant = _klantService.GetKlantByApplicationUserId(userId);
 
-                    return View("~/Views/Boekingen/Step02.cshtml", klant);
+                    return View(klant);
                 }
             }
 
             string klantJson = HttpContext.Session.GetString("KlantInfo");
             KlantViewModels sessionKlant = string.IsNullOrEmpty(klantJson) ? null : JsonConvert.DeserializeObject<KlantViewModels>(klantJson);
 
-            return View("~/Views/Boekingen/Step02.cshtml", sessionKlant);
+            return View(sessionKlant);
         }
 
         [HttpPost]
         public async Task<IActionResult> Step02(KlantViewModels klant)
         {
             DateTime eventDate = getEventDateFromSession();
+            if (eventDate == default) return RedirectToAction("Index", "Home");
 
-            if (eventDate == default)
+            if (!ModelState.IsValid)
             {
-                // Redirect if no event date is found
-                return RedirectToAction("Index", "Home");
+                return View(klant);
             }
-
-            ViewBag.EventDate = eventDate;
 
             if (klant.KlantkaartId == null) klant.KlantkaartId = 0;
 
@@ -102,23 +96,12 @@ namespace Prog6_Assessment_CodyBoelens.Controllers
         public async Task<IActionResult> Step03()
         {
             DateTime eventDate = getEventDateFromSession();
+            if (eventDate == default) return RedirectToAction("Index", "Home");
 
-            if (eventDate == default)
-            {
-                // Redirect if no event date is found
-                return RedirectToAction("Index", "Home");
-            }
+            List<int> selectedBeestjesIds = getSelectedBeestjesIdsFromSession();
 
-            ViewBag.EventDate = eventDate;
-
-            // Retrieve Klant information from session and deserialize it
-            string klantJson = HttpContext.Session.GetString("KlantInfo");
-            KlantViewModels klant = string.IsNullOrEmpty(klantJson) ? null : JsonConvert.DeserializeObject<KlantViewModels>(klantJson);
-
-            if (klant == null)
-            {
-                return RedirectToAction("Step02");
-            }
+            KlantViewModels klant = getKlantFromSession();
+            if (klant == null) return RedirectToAction("Step02");
 
             var beestjes = await _beestjeService.GetAllBeestjesAsync();
 
@@ -126,10 +109,11 @@ namespace Prog6_Assessment_CodyBoelens.Controllers
             {
                 Klant = klant,
                 Beestjes = beestjes,
-                Datum = eventDate 
+                Datum = eventDate,
+                SelectedBeestjesIds = selectedBeestjesIds
             };
 
-            return View("~/Views/Boekingen/Step03.cshtml", model);
+            return View(model);
         }
 
         [HttpPost]
@@ -150,14 +134,75 @@ namespace Prog6_Assessment_CodyBoelens.Controllers
                     ModelState.AddModelError(string.Empty, error);
                 }
 
-                return View("~/Views/Boekingen/Step03.cshtml", model);
+                return View(model);
             }
+
+            HttpContext.Session.SetString("SelectedBeestjes", JsonConvert.SerializeObject(model.SelectedBeestjesIds));
 
             // Proceed with further logic after validation is successful (e.g., save data, move to next step)
             return RedirectToAction("Step04");
         }
 
-        private DateTime getEventDateFromSession() 
+        [HttpGet]
+        public async Task<IActionResult> Step04()
+        {
+            DateTime eventDate = getEventDateFromSession();
+            if (eventDate == default) return RedirectToAction("Index", "Home");
+
+            List<int> selectedBeestjesIds = getSelectedBeestjesIdsFromSession();
+            if (!selectedBeestjesIds.Any()) return RedirectToAction("Step03");
+            var selectedBeestjes = await _beestjeService.GetBeestjesByIdsAsync(getSelectedBeestjesIdsFromSession());
+
+            KlantViewModels klant = getKlantFromSession();
+            if (klant == null) return RedirectToAction("Step02");
+
+            //calculate total price
+
+            // Create the ViewModel
+            var model = new Step04ViewModel
+            {
+                Klant = klant,
+                Beestjes = selectedBeestjes,
+                Datum = eventDate
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ConfirmBooking()
+        {
+            DateTime eventDate = getEventDateFromSession();
+            if (eventDate == default) return RedirectToAction("Index", "Home");
+
+            List<int> selectedBeestjesIds = getSelectedBeestjesIdsFromSession();
+            if (!selectedBeestjesIds.Any()) return RedirectToAction("Step03");
+            var selectedBeestjes = await _beestjeService.GetBeestjesByIdsAsync(getSelectedBeestjesIdsFromSession());
+
+            KlantViewModels klant = getKlantFromSession();
+            if (klant == null) return RedirectToAction("Step02");
+
+            // Create the ViewModel
+            var model = new Step04ViewModel
+            {
+                Klant = klant,
+                Beestjes = selectedBeestjes,
+                Datum = getEventDateFromSession()
+            };
+
+            //save booking
+
+            // Clear the session after booking
+            HttpContext.Session.Remove("selectedEventDate");
+            HttpContext.Session.Remove("KlantInfo");
+            HttpContext.Session.Remove("SelectedBeestjes");
+
+            // Redirect to a confirmation page
+            return RedirectToAction("Index", "Home");
+        }
+
+
+        private DateTime getEventDateFromSession()
         {
             // Retrieve the date from session
             var eventDateString = HttpContext.Session.GetString("selectedEventDate");
@@ -170,14 +215,26 @@ namespace Prog6_Assessment_CodyBoelens.Controllers
                 eventDate = parsedDate;
             }
 
+            ViewBag.EventDate = eventDate;
+
             return eventDate;
         }
 
+        private KlantViewModels getKlantFromSession()
+        {
+            string klantJson = HttpContext.Session.GetString("KlantInfo");
+            KlantViewModels klant = string.IsNullOrEmpty(klantJson) ? null : JsonConvert.DeserializeObject<KlantViewModels>(klantJson);
 
+            return klant;
+        }
 
+        private List<int> getSelectedBeestjesIdsFromSession()
+        {
+            string selectedBeestjesJson = HttpContext.Session.GetString("SelectedBeestjes");
+            List<int> selectedBeestjesIds = string.IsNullOrEmpty(selectedBeestjesJson) ? new List<int>() : JsonConvert.DeserializeObject<List<int>>(selectedBeestjesJson);
 
-
-
-
+            return selectedBeestjesIds;
+        }
     }
+
 }
--- Prog6_Assessment_CodyBoelens/Views/ViewModels/BoekingsViewModel/Step04ViewModel.cs
+++ Prog6_Assessment_CodyBoelens/Views/ViewModels/BoekingsViewModel/Step04ViewModel.cs
@@ -1,14 +1,12 @@
-﻿using Prog6_Assessment_CodyBoelens.Data.DbEntities;
-using Prog6_Assessment_CodyBoelens.Views.ViewModels.BeestjeViewModel;
+﻿using Prog6_Assessment_CodyBoelens.Views.ViewModels.BeestjeViewModel;
 using Prog6_Assessment_CodyBoelens.Views.ViewModels.KlantViewModel;
 
 namespace Prog6_Assessment_CodyBoelens.Views.ViewModels.BoekingsViewModel
 {
-    public class Step03ViewModel
+    public class Step04ViewModel
     {
         public KlantViewModels Klant { get; set; }
         public List<BeestjeViewModels> Beestjes { get; set; }
         public DateTime Datum { get; set; }
-        public List<int> SelectedBeestjesIds { get; set; } = new List<int>();
     }
 }
